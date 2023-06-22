fc0958e7a05f6544c79f328f09854bbbfc99b095
erickzz <erick.reis2@gmail.com>
2023-06-22 12:49:54 -0300

    Formatacao padrao, passar data formatada

--- src/app/(pages)/users/page.tsx
+++ src/app/(pages)/users/page.tsx
@@ -5,11 +5,13 @@ import getData from '@/app/firebase/db/getData';
 import { DocumentData } from 'firebase/firestore';
 import AuthContext from '@/app/context/auth-context';
 import { useRouter } from 'next/navigation';
+import UserCard from '../../components/UserCard';
+import classes from './users.module.css';
 
-type user = {
-  cidade: string;
-  email: string;
+type userType = {
   nome: string;
+  email: string;
+  cidade: string;
   profissao: string;
 };
 
@@ -31,13 +33,30 @@ const Users = () => {
     retrieveUsers();
   }, [ctx.isLogged]);
 
-  const renderUsers = usersData?.map((user: user) => (
-    <div key={user.email}>
-      <p>{user.nome}</p>
-      <p>{user.email}</p>
+  const test = {
+    nome: 'Erick3',
+    email: 'erick3@erick.com',
+    cidade: 'Cidade 3',
+    profissao: 'Nada3',
+  };
+  return (
+    <div className={classes.tableContainer}>
+      <table className={classes.tableUser}>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Nome</th>
+            <th>Cidade</th>
+            <th>Profissão</th>
+          </tr>
+        </thead>
+        <tbody>
+          <UserCard user={test} />
+          <UserCard user={test} />
+        </tbody>
+      </table>
     </div>
-  ));
-  return <div>{renderUsers}</div>;
+  );
 };
 
 export default Users;
---
+++ src/app/components/userCard.tsx
@@ -0,0 +1,15 @@
+import classes from './userCard.module.css';
+
+type user = { email: string; nome: string; cidade: string; profissao: string };
+const UserCard: React.FC<{ user: user }> = ({ user }) => {
+  return (
+    <tr className={classes.trUser}>
+      <td>{user.nome}</td>
+      <td>{user.nome}</td>
+      <td>{user.cidade}</td>
+      <td>{user.profissao}</td>
+    </tr>
+  );
+};
+
+export default UserCard;
